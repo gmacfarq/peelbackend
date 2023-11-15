009c78cf73031fba3ed4827825708b4b02caf63c
Graham MacFarquhar <gmacfarquhar57@gmail.com>
2023-11-15 10:40:31 -0800

    more marketplace

--- routes/products.js
+++ routes/products.js
@@ -4,9 +4,9 @@ const jsonschema = require("jsonschema");
 
 
 const express = require("express");
-const { ensureCorrectUserOrAdmin, ensureAdmin } = require("../middleware/auth");
+const { ensureAdmin } = require("../middleware/auth");
 const { BadRequestError } = require("../expressError");
-const Product = require("../models/products");
+const Product = require("../models/product");
 const productNewSchema = require("../schemas/productNew.json");
 const productUpdateSchema = require("../schemas/productUpdate.json");
 const productSearchSchema = require("../schemas/productSearch.json");
@@ -42,12 +42,18 @@ router.post("/", ensureAdmin, async function (req, res, next) {
 
 router.get("/", async function (req, res, next) {
   const q = req.query;
-  const nameLike = q.nameLike;
+  //const nameLike = q.nameLike;
   const validator = jsonschema.validate(
     q,
     productSearchSchema,
     { required: true });
-  const products = await Product.getAll();
+
+  if (!validator.valid) {
+    const errs = validator.errors.map(e => e.stack);
+    throw new BadRequestError(errs);
+  }
+
+  const products = await Product.findAll(q);
   return res.json({ products });
 });
 
--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,7 @@
 const jsonschema = require("jsonschema");
 
 const express = require("express");
-const { ensureCorrectUserOrAdmin, ensureAdmin } = require("../middleware/auth");
+const { ensureCorrectUserOrAdmin, ensureAdmin, ensureLoggedIn } = require("../middleware/auth");
 const { BadRequestError } = require("../expressError");
 const User = require("../models/user");
 const { createToken } = require("../helpers/tokens");
@@ -89,7 +89,7 @@ router.get("/buyers", ensureAdmin, async function (req, res, next) {
  * Authorization required: admin or same user-as-:username
  **/
 
-router.get("/:username", ensureCorrectUserOrAdmin, async function (req, res, next) {
+router.get("/:username", ensureLoggedIn, async function (req, res, next) {
   const user = await User.get(req.params.username);
   return res.json({ user });
 });
